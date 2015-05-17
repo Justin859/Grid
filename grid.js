2cba41d27a8fc86390fb22ad30dc14ec7a1e9581
Justin <justinhammond859@yahoo.com>
2015-05-17 23:38:51 +0200

    updated files

--- grid.js
+++ grid.js
@@ -1,5 +1,19 @@
+function grid(){
+  $('tr').append('<div></div>')
+};
+
+
+
 $(document).ready(function(){
-  $('div').mouseenter(function(){
+  for(var i = 0; i < 16; i++){
+    for(var j = 0; j < 16; j++){
+      grid();
+    }
+  }
+});
+
+$(document).ready(function(){
+  $('div').hover(function(){
     $(this).css('background-color', 'green');
   });
 });
