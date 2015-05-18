3492ec99b58f612ea067390a29e8c1a22cbf38a1
Justin <justinhammond859@yahoo.com>
2015-05-18 23:09:12 +0200

    updated files

--- grid.js
+++ grid.js
@@ -1,13 +1,13 @@
 
 $(document).ready(function(){
+  $('#createTable').append('<table>' + '</table>');
   for(var i = 0; i < 16; i++){
-  $('#createTable').append('<table>' + '<tr>' + '</tr>' + '</table>');
-    }
-    for(var j = 0; j < 16; j++){
-      $('tr').append('<td></td>');
-    }
+    $('table').append('<tr>' + '</tr>');
+  }
+  for(var j = 0; j < 16; j++){
+    $('tr').append('<td></td>');
+  }
 });
-
 
 $(document).ready(function(){
   $('td').hover(function(){
@@ -16,5 +16,19 @@ $(document).ready(function(){
 });
 
 function restart(){
-  var newGrid = prompt("How many rows?: ")
+  $('table').remove();
+  var newGrid = prompt("create new grid: ")
+
+  $(document).ready(function(){
+    $('#createTable').append('<table>' + '</table>');
+    for(var i = 0; i < newGrid; i++){
+      $('table').append('<tr>' + '</tr>');
+    }
+    for(var j = 0; j < newGrid; j++){
+      $('tr').append('<td></td>');
+    }
+      $('td').hover(function(){
+        $(this).css('background-color', 'green');
+      });
+  });
 }
